6b9dcf860def862a777b87f4c90f3a60a2818f27
rachtripoli <122343930+rachtripoli@users.noreply.github.com>
2024-12-08 21:14:19 +0000

    updates to layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,35 +12,14 @@ import tempfile
 import zipfile
 import s3fs
 
-
-
-# aws_access_key_id = st.secrets["aws_access_key_id"]
-# aws_secret_access_key = st.secrets["aws_secret_access_key"]
-# region = st.secrets["region"]
-
-# session = boto3.Session(profile_name="streamlit1")
-# s3 = session.client("s3",
-#             aws_access_key_id=aws_access_key_id,
-#             aws_secret_access_key=aws_secret_access_key,
-#             region_name=region)
-
-# url = s3.generate_presigned_url(
-#     ClientMethod="get_object",
-#     Params={"Bucket": "biascheck-232442840523-us-east-1", "Key": "distilbert_cls_model.h5"},
-#     ExpiresIn=3600
-# )
-
-# # Download and Load Model
-# response = requests.get(url)
-# with open("distilbert_cls_model.h5", "wb") as f:
-#     f.write(response.content)
-
+# set aws credentials
 aws_access_key_id=st.secrets["aws_access_key_id"]
 aws_secret_access_key=st.secrets["aws_secret_access_key"]
 region_name="us-east-1"
 BUCKET_NAME = "biascheck-232442840523-us-east-1"
 model_name = "classification_model_12082024.zip"
 
+# define s3 bucket functions
 def get_s3fs():
   return s3fs.S3FileSystem(key=aws_access_key_id, secret=aws_secret_access_key)
 
@@ -54,12 +33,15 @@ def s3_get_keras_model(model_name: str) -> keras.Model:
         zip_ref.extractall(f"{tempdir}/{model_name}")
     # Load the keras model from the temporary directory
     return keras.models.load_model(f"{tempdir}/{model_name}", custom_objects={"TFDistilBertModel": transformers.TFDistilBertModel})
-  
+
+# load model using previous function  
 cls_model=s3_get_keras_model(model_name)
 
+# load biascheck logo
 from PIL import Image
 img = Image.open("biaschecklogo.png").convert('RGBA')
 
+# set page configuration
 st.set_page_config(
     page_title="BIASCheck",
     page_icon=img,
@@ -74,7 +56,7 @@ st.set_page_config(
 
 col1, col2 = st.columns([0.09, 0.91], vertical_alignment="center")
 
-scale_factor = 2.5
+scale_factor = 0.1
 new_width = int(img.width * scale_factor)
 new_height = int(img.height * scale_factor)
 img_resized = img.resize((new_width, new_height), Image.Resampling.LANCZOS)
@@ -87,27 +69,47 @@ with col2:
 st.subheader("Minimizing subjectivity in language.")
 st.markdown("BIASCheck's objective is to assist writing professionals in checking their subjective biases in written language. Subjective biases are often unconscious and difficult to catch without a second pair of eyes. In the times when you are unable to call on someone else to read your work, call on BIASCheck!")
 
-text = st.text_input(label="Enter a text example to classify.", value=None, help="See documentation for tips!", placeholder="Text goes here")
+tab1, tab2 = st.tabs(["The Tool", "About"])
 
-parameters.LABEL_FONT_SIZE = "0 1.5rem"
 
-if text:
-    MAX_SEQUENCE_LENGTH = 512
-    tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-cased')
-    text_tokenized = tokenizer(text,
-              max_length=MAX_SEQUENCE_LENGTH,
-              truncation=True,
-              padding='max_length',
-              return_tensors='tf')
-    predictions = cls_model.predict(dict(text_tokenized))
-    predictions_flat = predictions.flatten()
-    predictions_list = predictions_flat.tolist()
-    predicted_labels = np.where(predictions > 0.5, "Neutral", "Biased")
-    labels_flattened = predicted_labels.flatten().tolist()
-    if predicted_labels == "Biased":
-        color = "#ffa421"
-    else:
-        color = "#21c354"
-    bias_score = 1 - predictions_list[0]
-    modified_prompt = (text, f"**BIASCheck**: *{labels_flattened[0]}* Score: {bias_score:.0%}", color)
-    annotated_text(modified_prompt)
+with tab1:
+    text = st.text_input(label="Enter a text example to classify.", value=None, help="See documentation for tips!", placeholder="Text goes here")
+
+    parameters.LABEL_FONT_SIZE = "0 1.5rem"
+
+    if text:
+        MAX_SEQUENCE_LENGTH = 512
+        tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-cased')
+        text_tokenized = tokenizer(text,
+                max_length=MAX_SEQUENCE_LENGTH,
+                truncation=True,
+                padding='max_length',
+                return_tensors='tf')
+        predictions = cls_model.predict(dict(text_tokenized))
+        predictions_flat = predictions.flatten()
+        predictions_list = predictions_flat.tolist()
+        predicted_labels = np.where(predictions > 0.5, "Neutral", "Biased")
+        labels_flattened = predicted_labels.flatten().tolist()
+        if predicted_labels == "Biased":
+            color = "#ffa421"
+        else:
+            color = "#21c354"
+        bias_score = 1 - predictions_list[0]
+        modified_prompt = (text, f"**BIASCheck**: *{labels_flattened[0]}* Score: {bias_score:.0%}", color)
+        annotated_text(modified_prompt)
+
+with tab2:
+    st.subheader("What is BIASCheck?", divider="blue")
+    st.write(f"**BIASCheck** is a multi-model based tool.\n"
+             "The tool is comprised of two key model components: a classification task and a neutralization task. ")
+    st.markdown("#### The Classification Task")
+    annotated_text("Your text first passes through a DistilBert-based classification task which classifies each sentence in your input",
+                      " as either ", ("Biased", "", "#ffa421"), " or ", ("Neutral","", "#21c354"), " along with an associated bias score. ",
+                      "The closer the score is to 100%, the more biased your text is.")
+    st.subheader("BIASCheck Best Practices", divider="blue")
+    st.write(f"In order to ensure a smooth experience with **BIASCheck**, we recommend the following practices:\n"
+             "1. **BIASCheck** can handle multiple sentences at a time, but all sentences must end in a period in order for the tool to properly detect.\n"
+             "2. ")
+    
+    
+    
