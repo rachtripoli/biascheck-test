980a9d9bcf7fa9df00521d71b40bb2a002c08711
rachtripoli <122343930+rachtripoli@users.noreply.github.com>
2024-12-08 04:12:03 +0000

    testing aws access

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,8 @@ import transformers
 import requests
 import boto3
 
-s3 = boto3.client("s3")
+session = boto3.Session(profile_name="streamlit1")
+s3 = session.client("s3")
 url = s3.generate_presigned_url(
     ClientMethod="get_object",
     Params={"Bucket": "biascheck-232442840523-us-east-1", "Key": "distilbert_cls_model.h5"},
