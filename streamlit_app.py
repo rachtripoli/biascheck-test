c2d9196864faca8652e4902c14dbb6cae9816f18
rachtripoli <122343930+rachtripoli@users.noreply.github.com>
2024-12-09 02:48:07 +0000

    added tabs added multi sentence abilities

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ with col2:
 st.subheader("Minimizing subjectivity in language.")
 st.markdown("BIASCheck's objective is to assist writing professionals in checking their subjective biases in written language. Subjective biases are often unconscious and difficult to catch without a second pair of eyes. In the times when you are unable to call on someone else to read your work, call on BIASCheck!")
 
-tab1, tab2 = st.tabs(["The Tool", "About"])
+tab1, tab2, tab3 = st.tabs(["The Tool", "About", "Privacy Statement"])
 
 
 with tab1:
@@ -78,38 +78,58 @@ with tab1:
     parameters.LABEL_FONT_SIZE = "0 1.5rem"
 
     if text:
+        text_input = text.split(". ")
         MAX_SEQUENCE_LENGTH = 512
         tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-cased')
-        text_tokenized = tokenizer(text,
+        text_tokenized = tokenizer(text_input,
                 max_length=MAX_SEQUENCE_LENGTH,
                 truncation=True,
                 padding='max_length',
                 return_tensors='tf')
         predictions = cls_model.predict(dict(text_tokenized))
-        predictions_flat = predictions.flatten()
-        predictions_list = predictions_flat.tolist()
+        
+        # predictions_flat = predictions.flatten()
+        # predictions_list = predictions_flat.tolist()
         predicted_labels = np.where(predictions > 0.5, "Neutral", "Biased")
-        labels_flattened = predicted_labels.flatten().tolist()
-        if predicted_labels == "Biased":
-            color = "#ffa421"
-        else:
-            color = "#21c354"
-        bias_score = 1 - predictions_list[0]
-        modified_prompt = (text, f"**BIASCheck**: *{labels_flattened[0]}* Score: {bias_score:.0%}", color)
-        annotated_text(modified_prompt)
+        # labels_flattened = predicted_labels.flatten().tolist()
+        # if predicted_labels == "Biased":
+        #     color = "#ffa421"
+        # else:
+        #     color = "#21c354"
+        #[f(x) if condition else g(x) for x in sequence]
+
+        color = ["#ffa421" if label == "Biased" else "#21c354" for label in predicted_labels]
+        bias_scores = 1 - predictions
+
+        for i in range(len(text_input)):
+            score = bias_scores[i].item() if isinstance(bias_scores[i], np.ndarray) else bias_scores[i]
+            label = predicted_labels[i].item() if isinstance(predicted_labels[i], np.ndarray) else predicted_labels[i]
+            modified_prompt = (text_input[i], f"**BIASCheck**: *{label}* Score: {score:.0%}", color[i])
+            annotated_text(modified_prompt)
+        # modified_prompt = (text, f"**BIASCheck**: *{labels_flattened[0]}* Score: {bias_score:.0%}", color)
+        # annotated_text(modified_prompt)
 
 with tab2:
     st.subheader("What is BIASCheck?", divider="blue")
-    st.write(f"**BIASCheck** is a multi-model based tool.\n"
+    st.write(f"**BIASCheck** is a multi-model based end-to-end Bias Mitigation system.\n"
              "The tool is comprised of two key model components: a classification task and a neutralization task. ")
     st.markdown("#### The Classification Task")
     annotated_text("Your text first passes through a DistilBert-based classification task which classifies each sentence in your input",
                       " as either ", ("Biased", "", "#ffa421"), " or ", ("Neutral","", "#21c354"), " along with an associated bias score. ",
                       "The closer the score is to 100%, the more biased your text is.")
+    st.markdown("#### The Neutralization Task")
+    annotated_text("After your text has been classified, if the text is considered ", ("Biased", "", "#ffa421"),
+                   " the text is passed through a fine-tuned Llama model to remove subjectivity. The neutralized text is displayed under the classification.")
+    st.markdown("For more information on our data pipeline, technical approach, and decision making, please go to our website: [BIASCheck](https://sites.ischool.berkeley.edu/biascheck/)")
+    st.markdown("")
     st.subheader("BIASCheck Best Practices", divider="blue")
     st.write(f"In order to ensure a smooth experience with **BIASCheck**, we recommend the following practices:\n"
              "1. **BIASCheck** can handle multiple sentences at a time, but all sentences must end in a period in order for the tool to properly detect.\n"
              "2. ")
+    
+    with tab3:
+       st.subheader("Privacy Statement", divider = "gray")
+       st.markdown("#### Privacy Statement")
     
     
     
